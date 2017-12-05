eff2644c42d65520b762569691379a801ada1b5f
Yang-Hsiang Chang <leo424y@gmail.com>
2017-12-05 12:00:47 +0800

    search wiki if chinese in 4 words

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
-  def only_one_english_word(string)
-    (string.length == string.bytes.to_a.length) && (string.length == string.gsub(/[[:space:]]/, '').length)
+  def only_one_english_word(talk)
+    if (talk.topic.length == talk.topic.bytes.to_a.length) && (talk.topic.length == talk.topic.gsub(/[[:space:]]/, '').length)
+      "https://simple.wikipedia.org/wiki/#{talk.topic}"
+    elsif (talk.topic.length < 5) && (talk.topic.length == talk.topic.gsub(/[[:space:]]/, '').length)
+      "https://zh-tw.wikipedia.org/wiki/#{talk.topic}"
+    else
+      new_talk_path(f: talk.id)
+    end
   end
 end

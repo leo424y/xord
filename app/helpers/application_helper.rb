60386d3060364ab1d3f4f0a0291e98e35957bdec
Yang-Hsiang Chang <leo424y@gmail.com>
2017-12-05 11:23:24 +0800

    link to wiki in one english word

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def only_one_english_word(string)
+    (string.length == string.bytes.to_a.length) && (string.length == string.gsub(/[[:space:]]/, '').length)
+  end
 end

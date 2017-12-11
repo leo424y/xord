f96ca10198638cced0921918a51194e2429338e6
Yang-Hsiang Chang <leo424y@gmail.com>
2017-12-11 14:41:21 +0800

    .

--- spec/models/talk_spec.rb
+++ spec/models/talk_spec.rb
@@ -1,16 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Talk, type: :model do
-  # 常數 constant
-  # 關聯 associations
-  # 驗證 validations
-  # Scope
-  # 類別方法 class method
-  # 實體方法 instance method
-  describe "#parent_talk" do
-    it "return right content" do
-      expect(Talk.parent_talk).to include("dog")
-      Talk.delet_all
-    end
-  end
 end

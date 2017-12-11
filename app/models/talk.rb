1e20b2e168dc73d7163709c249f6073329dc80d6
Yang-Hsiang Chang <leo424y@gmail.com>
2017-12-11 11:54:27 +0800

    .

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,16 +1,16 @@
-require 'csv'
+# require 'csv'
 
 class Talk < ApplicationRecord
   validates_presence_of :topic
-  def self.to_csv
-    attributes = %w{topic}
-
-    CSV.generate(headers: true) do |csv|
-      csv << attributes
-
-      all.each do |user|
-        csv << attributes.map{ |attr| user.send(attr) }
-      end
-    end
-  end
+  # def self.to_csv
+  #   attributes = %w{topic}
+  #
+  #   CSV.generate(headers: true) do |csv|
+  #     csv << attributes
+  #
+  #     all.each do |user|
+  #       csv << attributes.map{ |attr| user.send(attr) }
+  #     end
+  #   end
+  # end
 end

be03406e0c80915d51fe30327de18ff4cdf32ce9
Yang-Hsiang Chang <leo424y@gmail.com>
2017-12-11 14:45:28 +0800

    put parent_talk in model

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,7 +1,20 @@
 # require 'csv'
 
 class Talk < ApplicationRecord
-  validates_presence_of :topic
+  # validates_presence_of :topic
+
+  def self.parent_talk
+    parent_id = from
+    @parent_ids << sub_id
+    if parent_id.present?
+      @parent_ids << parent_id
+      parent_talk(parent_id)
+    else
+      ids = @parent_ids
+      @parent_ids =[]
+      where(id: ids).pluck(:id, :topic)
+    end
+  end
   # def self.to_csv
   #   attributes = %w{topic}
   #

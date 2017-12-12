59d6c20cab35baf5e6f3c8624be5bada3a3e0c95
Yang-Hsiang Chang <leo424y@gmail.com>
2017-12-12 09:55:53 +0800

    .

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 RSpec.describe ApplicationHelper do
   include ApplicationHelper
   before(:all) {@talk_id = 1}
-  # it { link_by_topic(f: @talk_id).should == "/talks/new" }
+  ## it { link_by_topic(f: @talk_id).should == "/talks/new" }
 end

a09da5594d21bb9aeba1ceedc1c5c4ec9f7c86f5
Yang-Hsiang Chang <leo424y@gmail.com>
2017-12-12 11:47:52 +0800

    lograge

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem "lograge"
+
 gem 'guard-rspec', require: false
 
 gem 'coveralls', require: false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,16 @@
 Rails.application.configure do
+
+  config.lograge.enabled = true
+  # add time to lograge
+  config.lograge.custom_options = lambda do |event|
+    { time: event.time }
+  end
+
+  config.lograge.keep_original_rails_log = true
+
+  config.lograge.logger = ActiveSupport::Logger.new "#{Rails.root}/log/lograge_#{Rails.env}.log"
+
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
